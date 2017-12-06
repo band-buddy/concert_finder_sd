f543e001883b30006a2e685b32691f416526a431
learn <learn@learn09.local>
2017-12-06 09:34:24 -0800

    events_list branch first commit

---
+++ app/javascript/pages/events_list.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import Events from 'components/event.js'
+
+export default class EventsList extends Component {
+  render(){
+    return(
+      <div>
+        <Events />
+      </div>
+    )
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'events' => 'events_list#index'
+
+  get '/events' => 'events_list#events'
 
   root to: 'index#index'
 end
