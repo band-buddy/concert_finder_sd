b4aaa6f6064e919f0853e2ead863e2311ca3004c
learn <learn@learn17.local>
2017-12-06 17:09:34 -0800

    Added cards to homepage, rearranged the top navigation bar to show unique ids

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w(navbar.css)
 Rails.application.config.assets.precompile += %w(home.css)
+Rails.application.config.assets.precompile += %w(appinfo.css)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
