9751e4d77e49ffb7766dfe71a00154f35361b51e
learn <learn@learn05.local>
2017-12-14 13:10:23 -0800

    db changed for google maps

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171213194703) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,16 +27,6 @@ ActiveRecord::Schema.define(version: 20171213194703) do
     t.date "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "username"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.date "date_of_birth"
-    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,17 +46,14 @@ ActiveRecord::Schema.define(version: 20171213194703) do
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
-
+    t.string "description"
+    t.string "phone_number"
     t.string "provider"
     t.string "uid"
-
-    t.string "phone_number"
-    t.string "description"
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
