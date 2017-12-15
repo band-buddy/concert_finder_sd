c70c806f98a8595e05d25ba341f318d713731449
learn <learn@learn05.local>
2017-12-15 12:05:37 -0800

    after merge

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -4,6 +4,13 @@ import Footer from 'components/footer.js'
 import AppInfo from 'components/appinfo.js';
 
 export default class Home extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      // events: this.props.events ? this.props.events.events.event : []
+      events: this.props.events
+    }
+  }
 
   render (){
     return(
@@ -14,7 +21,8 @@ export default class Home extends Component{
             <h2 className="fadeText">Find the best concerts in San Diego.<br />
             Meet new friends.</h2>
             <form action="events" method="get">
-              <input className="homeSearch" type='text' placeholder="Search for events by band name or venue"/>
+              <input className="homeSearch" name="keyword_search" type='text' placeholder="Search for events by band name or venue"/><br></br>
+              <button className="homeButton" type="submit">Search Events!</button>
             </form>
           </div>
           <div className="cardsSection">
