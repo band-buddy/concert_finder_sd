b778abf652e9ee054c8ca0de22fe08bb48278657
learn <learn@learn15.local>
2017-12-15 16:56:11 -0800

    Fixed logout

--- app/javascript/components/about.js
+++ app/javascript/components/about.js
@@ -7,28 +7,28 @@ export default class About extends Component {
       profile: [
         {
           name: "Greg Foreman",
-          image: "#",
-          description: "Add Description."
-        },
-        {
-          name: "Veronica Basch",
-          image: "#",
-          description: "Add Description."
+          image: "/assets/greg.jpg",
+          description: "Greg moved to San Diego from Chicago to pursue his careeer in Web Development. When he's not busy creating cool apps, he likes to surf, ride his bike, and dabble in photography. And, he loves music."
         },
         {
           name: "Ryan Wehr",
-          image: "#",
-          description: "Add Description."
+          image: "/assets/ryan.jpg",
+          description: "Ryan is our San Diegan native. When he's not busy creating cool apps, well, he's busy writing other code in his Minecraft server. But, like the rest of us, he digs music."
+        },
+        {
+          name: "Veronica Basch",
+          image: "/assets/vero.jpg",
+          description: "Originally from Michigan, Veronica lived all over the place before landing in San Diego. When she's not busy creating cool apps, she can be found in the ocean surfing, hanging out at the beach, or going to concerts. And of course, she loves music."
         },
         {
           name: "Tim Petrone",
-          image: "#",
-          description: "Add Description."
+          image: "/assets/Tim-Wedding-Dance.jpg",
+          description: "Tim hails from New York but left that hectic life to dive in to the hectic web development life in San Diego. When he's not busy creating cool apps, he is watching baseball, watching movies, or playing video games. Or listening to music!"
         },
         {
           name: "Kiah Stroud",
-          image: "#",
-          description: "Add Description."
+          image: "/assets/kiah.jpg",
+          description: "He wasn't born there, but Kiah was in Washington State before coming to San Diego 6 years go. When he's not busy creating cool apps, he is into books, audiobooks, and podcasts. And, you guessed it, he likes music!"
         },
       ]
     }
@@ -38,13 +38,13 @@ export default class About extends Component {
     let people = this.state.profile.map(function(p, i) {
       return (
         <div className={"event-card"} key={i}>
-            <div className={"flex-card-image"}>
+            <div className={"event-card-image"}>
               <img src={p.image} alt="Show Image" />
             </div>
-            <div className={"flex-card-heading"}>
+            <div className={"event-card-heading"}>
               <h3>{p.name}</h3>
             </div>
-            <div className={"flex-card-content"}>
+            <div className={"event-card-content"}>
               <p>{p.description}</p>
             </div>
         </div>
