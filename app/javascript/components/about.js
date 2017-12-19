6c0464e913ce60d470fd1be6f8d0c23f40d8031b
learn <learn@learn17.local>
2017-12-19 10:53:13 -0800

    changes

--- app/javascript/components/about.js
+++ app/javascript/components/about.js
@@ -52,6 +52,7 @@ export default class About extends Component {
             <div className={"event-card-content"}>
               <p>{p.description}</p><br />
               <p>{p.current}</p>
+              <iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365760815&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
             </div>
         </div>
       )
